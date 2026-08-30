cf47bdad907c012c74766c4cfc3cd061c020f604
krishna kumar <krishna462003@gmail.com>
2026-08-30 13:45:24 +0530

    fix: simplify next.config.mjs to use turbopack, remove webpack dependency

--- next.config.mjs
+++ next.config.mjs
@@ -1,48 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    // Next.js 15+ nests rules inside a `oneOf` array.
-    // We need to find and modify the SVG rule within that structure,
-    // then prepend our SVGR rule so it takes priority.
-    config.module.rules.forEach((rule) => {
-      if (rule.oneOf) {
-        // Find and disable the default SVG handler inside oneOf
-        rule.oneOf.forEach((r) => {
-          if (
-            r.test instanceof RegExp &&
-            r.test.test(".svg") &&
-            !r.resourceQuery
-          ) {
-            r.resourceQuery = /url/; // limit it to ?url imports only
-          }
-        });
-
-        // Prepend our SVGR rule at the beginning of oneOf so it wins
-        rule.oneOf.unshift({
-          test: /\.svg$/i,
-          issuer: /\.[jt]sx?$/,
-          resourceQuery: { not: /url/ },
-          use: [
-            {
-              loader: "@svgr/webpack",
-              options: {
-                svgoConfig: {
-                  plugins: [
-                    {
-                      name: "preset-default",
-                      params: { overrides: { removeViewBox: false } },
-                    },
-                  ],
-                },
-              },
-            },
-          ],
-        });
-      }
-    });
-
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
